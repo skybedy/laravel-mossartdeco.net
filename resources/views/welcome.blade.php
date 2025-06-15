2ef48dc439287905238d7db122b5e5425f71e74c
skybedy <skybedy@gmail.com>
2025-06-15 17:41:23 +0100

    Initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/             css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+         @vite(['resources/css/app.css', 'resources/js/app.js'])
+         
+    </head>
+   <body class="bg-red-500">
+    <div class="text-white text-2xl p-4">Tailwind funguje! 🎉</div>
+</body>
+               
+</html>
